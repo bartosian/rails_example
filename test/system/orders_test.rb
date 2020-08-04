3ec41e5b904e4bd314e98993502e0be72f3cd3c7
bartosian <bartosian1989@gmail.com>
2020-08-04 07:56:09 -0400

    Added system tests for cart's functionality

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,9 +1,36 @@
 require "application_system_test_case"
 
 class OrdersTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit orders_url
-  #
-  #   assert_selector "h1", text: "Order"
-  # end
+	test 'check credit card number' do
+		visit store_index_url
+		
+		first('.catalog li').click_on 'Add to Cart'
+
+		click_on 'Checkout'
+
+		fill_in 'order_name', with: 'Dave Thomas'
+		fill_in 'order_address', with: '123 Main Street'
+		fill_in 'order_email', with: 'dave@example.com'
+
+		assert_no_selector "#order_routing_number"
+		assert_no_selector "#order_account_number"
+
+		select 'Credit card', from: 'pay_type'
+
+		assert_selector "#order_credit_card_number"
+	end
+	
+	test "check showing cart after adding product to it" do
+		visit store_index_url
+		first('.catalog li').click_on 'Add to Cart'
+
+		assert_selector '#cart'
+	end
+	
+	test "check highlighting after adding products to cart" do
+		visit store_index_url
+		first('.catalog li').click_on 'Add to Cart'
+		
+		assert_selector '#cart .line-item-highlight'
+	end
 end
