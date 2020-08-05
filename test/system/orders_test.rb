5ae1798cf3067aa96855c39f1f3320ab1c0f8847
bartosian <bartosian1989@gmail.com>
2020-08-05 08:50:24 -0400

    Added active job for payment processing

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,36 +1,56 @@
 require "application_system_test_case"
 
+
 class OrdersTest < ApplicationSystemTestCase
-	test 'check credit card number' do
-		visit store_index_url
-		
-		first('.catalog li').click_on 'Add to Cart'
+  include ActiveJob::TestHelper
+    
 
-		click_on 'Checkout'
+  test "check routing number" do
 
-		fill_in 'order_name', with: 'Dave Thomas'
-		fill_in 'order_address', with: '123 Main Street'
-		fill_in 'order_email', with: 'dave@example.com'
+    LineItem.delete_all
+    Order.delete_all
 
-		assert_no_selector "#order_routing_number"
-		assert_no_selector "#order_account_number"
+    visit store_index_url
 
-		select 'Credit card', from: 'pay_type'
+    visit store_index_url
 
-		assert_selector "#order_credit_card_number"
-	end
-	
-	test "check showing cart after adding product to it" do
-		visit store_index_url
-		first('.catalog li').click_on 'Add to Cart'
+    first('.catalog li').click_on 'Add to Cart'
 
-		assert_selector '#cart'
-	end
-	
-	test "check highlighting after adding products to cart" do
-		visit store_index_url
-		first('.catalog li').click_on 'Add to Cart'
-		
-		assert_selector '#cart .line-item-highlight'
-	end
+    click_on 'Checkout'
+
+    fill_in 'order_name', with: 'Dave Thomas'
+    fill_in 'order_address', with: '123 Main Street'
+    fill_in 'order_email', with: 'dave@example.com'
+
+    assert_no_selector "#order_routing_number"
+
+    select 'Check', from: 'pay_type'
+
+
+    assert_selector "#order_routing_number"
+
+    fill_in "Routing #", with: "123456"
+    fill_in "Account #", with: "987654"
+
+    perform_enqueued_jobs do
+      click_button "Place Order"
+    end
+
+    orders = Order.all
+    assert_equal 1, orders.size
+
+    order = orders.first
+
+    assert_equal "Dave Thomas",      order.name
+    assert_equal "123 Main Street",  order.address
+    assert_equal "dave@example.com", order.email
+    assert_equal "Check",            order.pay_type
+    assert_equal 1, order.line_items.size
+
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ["dave@example.com"],                 mail.to
+    assert_equal 'Sam Ruby <depot@example.com>',       mail[:from].value
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+
+  end 
 end
