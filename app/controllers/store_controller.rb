a303a443bf3d092dec9973d8f45858bbfe3b09cf
bartosian <bartosian1989@gmail.com>
2020-08-07 09:33:45 -0400

    Added switcher for languages

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -5,6 +5,12 @@ class StoreController < ApplicationController
   before_action :set_cart  
 
   def index
+	if params[:set_locale]
+		redirect_to store_index_url(locale: params[:set_locale])
+	else
+		@products = Product.order(:title)
+	end
+
 	if session[:counter].nil?
 		session[:counter] = 1
          else
